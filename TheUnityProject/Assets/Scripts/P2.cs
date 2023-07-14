a264a969a13b6a3d83508ffbc9cc3b039855a25c
Deltager <teknisk+GDC2023-GR1@game.unf.dk>
2023-07-14 21:22:54 +0200

    Movement og lys (virker kinda)
    Movement virker, lys er sådan ikke rigtig... nej

---
+++ TheUnityProject/Assets/Scripts/P2.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2 : MonoBehaviour
+{
+    private float hMove;
+    private float vMove;
+
+    [SerializeField] private float moveSpeed = 1f;
+
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hMove = Input.GetAxis("Horizontal_P2");
+        vMove = Input.GetAxis("Vertical_P2");
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(hMove * moveSpeed, rb.velocity.y, vMove * moveSpeed);
+    }
+    
+    private void OnTriggerStay(Collider other)
+    {
+
+        if(other.CompareTag("Light") && Input.GetKeyDown(KeyCode.RightControl))
+        {
+            Debug.Log("av det brander");
+            other.transform.GetChild(1).gameObject.SetActive(false);
+        }
+    }
+}

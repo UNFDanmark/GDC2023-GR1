aa90eb010f71b94dcca50e9889c9dbefadbd23a5
Deltager <teknisk+GDC2023-GR1@game.unf.dk>
2023-07-19 09:22:05 +0200

    CountDown meter is done

--- TheUnityProject/Assets/Scripts/CountDownScript.cs
+++ TheUnityProject/Assets/Scripts/CountDownScript.cs
@@ -7,12 +7,16 @@ public class CountDownScript : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI counter;
     [SerializeField] private int startTime;
-    [SerializeField] private int time;
+    [SerializeField] private int timeSeconds;
+    [SerializeField] private int timeMinutes;
+
+    private bool gameEnded = false;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeMinutes = startTime / 60;
+        StartCoroutine(Countdown());
     }
 
     // Update is called once per frame
@@ -20,4 +24,29 @@ public class CountDownScript : MonoBehaviour
     {
         
     }
+    
+    private IEnumerator Countdown()
+    {
+        while (true)
+        {
+            if (timeSeconds == 0)
+            {
+                if (timeMinutes == 0)
+                {
+                    gameEnded = true;
+                    yield break;
+                }
+                
+                timeSeconds = 60;
+                timeMinutes--;
+            }
+ 
+            timeSeconds--;
+            
+            counter.text = timeMinutes.ToString("D2") + ":" + timeSeconds.ToString("D2");
+            yield return new WaitForSeconds(1f);
+
+        }
+    }
+
 }
